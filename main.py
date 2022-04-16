8d5aede1d9f77771597a868204d5c0a8600eeb47
Rami Zeid <ra.zein.zein@gmail.com>
2022-04-16 12:42:25 -0400

    Re-organized main script and added more comments

--- main.py
+++ main.py
@@ -45,148 +45,9 @@ async def on_ready():
     print('TTS VC initialized.')
 
 
-@client.command()
-async def join(ctx, *, channel_id=None):
-    """
-    Command that allows the bot to join a voice channel (either by ID, or automatically joins the channel that the user is in).
-    """
-    author_id = ctx.author.id
-
-    if (author_id in user_ids) or (len(user_ids) == 0):
-        if channel_id is None:
-            if ctx.author.voice:
-                channel = ctx.author.voice.channel
-                await channel.connect()
-                await ctx.guild.change_voice_state(channel=channel, self_deaf=True)
-                await ctx.send(f"Joined voice channel #{channel}.")
-            else:
-                await ctx.send("You are not in a voice channel.")
-        else:
-            try:
-                channel_id = eval(channel_id)
-                channel = client.get_channel(channel_id)
-                await channel.connect()
-                await ctx.guild.change_voice_state(channel=channel, self_deaf=True)
-                await ctx.send(f"Joined voice channel #{channel}.")
-            except:
-                await ctx.send("Invalid channel id.")
-
-
-@client.command()
-async def leave(ctx):
-    """
-    Command that allows the bot to leave the channel it is currently in. Calling this command will also reset the system 
-    (see the reset command for more information).
-    """
-    author_id = ctx.author.id
-
-    if (author_id in user_ids) or (len(user_ids) == 0):
-        try:
-            channel = ctx.voice_client.channel
-            await ctx.voice_client.disconnect()
-            await ctx.send(f"Left voice channel #{channel}.")
-
-            cmd = client.get_command("reset")
-            await cmd(ctx)
-        except:
-            await ctx.send("Not in a voice channel.")
-
-
-@client.command()
-async def accent(ctx, *, accent_input):
-    """
-    Command that allows the user to change the bot's accent.
-    """
-    global ibm_watson_accent
-    global watson_accents_list
-    author_id = ctx.author.id
-
-    if (author_id in user_ids) or (len(user_ids) == 0):
-        for i, template_accent in enumerate(watson_accents_list):
-            watson_accents_list[i] = template_accent.lower()
-
-        if accent_input.lower() == "default":
-            ibm_watson_accent = IBM_WATSON_DEFAULT_ACCENT
-            await ctx.send(f'Changed the bot\'s accent to "{accent_input}".')
-        elif accent_input.lower() in watson_accents_list:
-            print(ibm_watson_accent)
-            ibm_watson_accent = accent_input
-            await ctx.send(f'Changed the bot\'s accent to "{accent_input}".')
-        else:
-            await ctx.send(f'Invalid accent.')
-
-
-@client.command()
-async def accents(ctx):
-    """
-    Command that allows the user to get a list of available bot accents.
-    """
-    author_id = ctx.author.id
-    accents_list_string = "```\nList of accents: \n \n"
-
-    if (author_id in user_ids) or (len(user_ids) == 0):
-        accents_list_string += f"- default ({IBM_WATSON_DEFAULT_ACCENT})\n"
-
-        for current_accent in watson_accents_list:
-            accents_list_string += f"- {current_accent}\n"
-
-        accents_list_string += "\nMore information here: https://cloud.ibm.com/docs/text-to-speech?topic=text-to-speech-voices```"
-
-        await ctx.send(accents_list_string)
-
-
-def is_connected(ctx):
-    """
-    Function that checks if the bot is connected to a voice channel.
-    """
-    voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
-    return voice_client and voice_client.is_connected()
-
-
-@client.command()
-async def reset(ctx):
-    """
-    Command that resets the system when called (deletes the generated MP3 files and changes the bot's accent to its 
-    default value).
-    """
-    global message_count
-    global ibm_watson_accent
-    author_id = ctx.author.id
-    folder_name = "mp3_messages"
-
-    if (author_id in user_ids) or (len(user_ids) == 0):
-        for file in os.listdir(folder_name):
-            os.remove(f'{folder_name}/{file}')
-
-        message_count = 0
-        ibm_watson_accent = IBM_WATSON_DEFAULT_ACCENT
-
-        await ctx.send("Successfully reset the system.")
-
-
-@client.command()
-async def play(ctx, *, mp3_file):
-    """
-    Command that translates text to MP3 files which are then played by the bot in the desired voice channel.
-    """
-    guild = ctx.guild
-    voice_client = discord.utils.get(client.voice_clients, guild=guild)
-
-    ffmpeg_location = "bin/ffmpeg.exe"
-    mp3_file_location = mp3_file
-
-    if not voice_client.is_playing():
-        # On Windows
-        if version == "W":
-            voice_client.play(discord.FFmpegPCMAudio(executable=ffmpeg_location, source=mp3_file_location))
-        elif version == "L":
-            # On Linux, make sure to have FFMPEG installed
-            voice_client.play(discord.FFmpegPCMAudio(source=mp3_file_location))
-        else:
-            print("Invalid system version")
-            await ctx.send("Invalid system version")
-
-
+# ---------------------------------------
+# On_message Event
+# ---------------------------------------
 @client.event
 async def on_message(message):
     """
@@ -216,5 +77,169 @@ async def on_message(message):
         message_count += 1
 
     await client.process_commands(message)
+
+
+# ---------------------------------------
+# Join Command
+# ---------------------------------------
+@client.command()
+async def join(ctx, *, channel_id=None):
+    """
+    Command that allows the bot to join a voice channel (either by ID, or automatically joins the channel that the user is in).
+    """
+    author_id = ctx.author.id
+
+    if (author_id in user_ids) or (len(user_ids) == 0):
+        if channel_id is None:
+            if ctx.author.voice:
+                channel = ctx.author.voice.channel
+                await channel.connect()
+                await ctx.guild.change_voice_state(channel=channel, self_deaf=True)
+                await ctx.send(f"Joined voice channel #{channel}.")
+            else:
+                await ctx.send("You are not in a voice channel.")
+        else:
+            try:
+                channel_id = eval(channel_id)
+                channel = client.get_channel(channel_id)
+                await channel.connect()
+                await ctx.guild.change_voice_state(channel=channel, self_deaf=True)
+                await ctx.send(f"Joined voice channel #{channel}.")
+            except:
+                await ctx.send("Invalid channel id.")
+
+
+# ---------------------------------------
+# Leave Command
+# ---------------------------------------
+@client.command()
+async def leave(ctx):
+    """
+    Command that allows the bot to leave the channel it is currently in. Calling this command will also reset the system 
+    (see the reset command for more information).
+    """
+    author_id = ctx.author.id
+
+    if (author_id in user_ids) or (len(user_ids) == 0):
+        try:
+            channel = ctx.voice_client.channel
+            await ctx.voice_client.disconnect()
+            await ctx.send(f"Left voice channel #{channel}.")
+
+            cmd = client.get_command("reset")
+            await cmd(ctx)
+        except:
+            await ctx.send("Not in a voice channel.")
+
+
+# ---------------------------------------
+# Accent Command
+# ---------------------------------------
+@client.command()
+async def accent(ctx, *, accent_input):
+    """
+    Command that allows the user to change the bot's accent.
+    """
+    global ibm_watson_accent
+    global watson_accents_list
+    author_id = ctx.author.id
+
+    if (author_id in user_ids) or (len(user_ids) == 0):
+        for i, template_accent in enumerate(watson_accents_list):
+            watson_accents_list[i] = template_accent.lower()
+
+        if accent_input.lower() == "default":
+            ibm_watson_accent = IBM_WATSON_DEFAULT_ACCENT
+            await ctx.send(f'Changed the bot\'s accent to "{accent_input}".')
+        elif accent_input.lower() in watson_accents_list:
+            print(ibm_watson_accent)
+            ibm_watson_accent = accent_input
+            await ctx.send(f'Changed the bot\'s accent to "{accent_input}".')
+        else:
+            await ctx.send(f'Invalid accent.')
+
+
+# ---------------------------------------
+# Accents Command
+# ---------------------------------------
+@client.command()
+async def accents(ctx):
+    """
+    Command that allows the user to get a list of available bot accents.
+    """
+    author_id = ctx.author.id
+    accents_list_string = "```\nList of accents: \n \n"
+
+    if (author_id in user_ids) or (len(user_ids) == 0):
+        accents_list_string += f"- default ({IBM_WATSON_DEFAULT_ACCENT})\n"
+
+        for current_accent in watson_accents_list:
+            accents_list_string += f"- {current_accent}\n"
+
+        accents_list_string += "\nMore information here: https://cloud.ibm.com/docs/text-to-speech?topic=text-to-speech-voices```"
+
+        await ctx.send(accents_list_string)
+
+
+# ---------------------------------------
+# Reset Command
+# ---------------------------------------
+@client.command()
+async def reset(ctx):
+    """
+    Command that resets the system when called (deletes the generated MP3 files and changes the bot's accent to its 
+    default value).
+    """
+    global message_count
+    global ibm_watson_accent
+    author_id = ctx.author.id
+    folder_name = "mp3_messages"
+
+    if (author_id in user_ids) or (len(user_ids) == 0):
+        for file in os.listdir(folder_name):
+            os.remove(f'{folder_name}/{file}')
+
+        message_count = 0
+        ibm_watson_accent = IBM_WATSON_DEFAULT_ACCENT
+
+        await ctx.send("Successfully reset the system.")
+
+
+# ---------------------------------------
+# Play Command
+# ---------------------------------------
+@client.command()
+async def play(ctx, *, mp3_file):
+    """
+    Command that translates text to MP3 files which are then played by the bot in the desired voice channel.
+    """
+    guild = ctx.guild
+    voice_client = discord.utils.get(client.voice_clients, guild=guild)
+
+    ffmpeg_location = "bin/ffmpeg.exe"
+    mp3_file_location = mp3_file
+
+    if not voice_client.is_playing():
+        # On Windows
+        if version == "W":
+            voice_client.play(discord.FFmpegPCMAudio(executable=ffmpeg_location, source=mp3_file_location))
+        elif version == "L":
+            # On Linux, make sure to have FFMPEG installed
+            voice_client.play(discord.FFmpegPCMAudio(source=mp3_file_location))
+        else:
+            print("Invalid system version")
+            await ctx.send("Invalid system version")
+
+
+# ---------------------------------------
+# Is_connected Helper Function
+# ---------------------------------------
+def is_connected(ctx):
+    """
+    Function that checks if the bot is connected to a voice channel.
+    """
+    voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+    return voice_client and voice_client.is_connected()
+
 
 client.run(DISCORD_TOKEN)
